a8bc7c7a42988822f13fde106594f6e5861a25d6
Megan Coumbe <mcoum@uic.edu>
2026-04-18 15:04:41 -0500

    Add files via upload

---
+++ Final3.cpp
@@ -0,0 +1,45 @@
+//Megan Coumbe
+//CIS 223 400
+//Professor Wang
+//12 / 13 / 2023
+//Final 3 Q5
+#include <iostream>
+using namespace std; 
+double Median(int *array, int numElements)
+{
+	int sum = 0; 
+	for (int i = 0; i < numElements; i++)
+	{
+		sum += array[i];
+	}
+	double total = (double)sum / (double)numElements;
+	return (total);
+}
+int main()
+{
+	//Initializing
+	int *arr; 
+	int size = 4;
+	int num = 2;
+	arr = new int[size];
+	//Premade EX
+	cout << "Pre made A array of values of 2: ";
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = num;
+	}
+	double average = Median(arr, size);
+	cout << average << endl;
+	//Prompt Size
+	cout << "A array of user choice: ";
+	//Input
+	for (int i = 0; i < size; i++)
+	{
+		cin >> num;
+		arr[i] = num;
+	}
+	//Call
+	Median(arr, size);
+	//Print
+	cout << Median(arr, size) << endl;
+}
